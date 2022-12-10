ff0ad29ec9ad01e32a9d86c0d21599d522126adf
Dmitry <panfilovdima1@yandex.ru>
2022-12-10 14:12:32 +0300

    HW4 done

---
+++ task25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите цикл, которые принимает на вход два числа (А и В) и возводит число А в степерь В.
+Console.WriteLine("Введите число А");
+int number =int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число B");
+int numberDegree =int.Parse(Console.ReadLine()!);
+
+int poweredNumber = Power(number, numberDegree);
+Console.WriteLine(poweredNumber);
+
+int Power(int A, int B)
+{
+    int result = 1;
+    if (B == 0)
+    {
+        return result;
+    }
+    
+    else
+    {
+        for (int i = 1; i <= B; i++)   
+        {
+            result = result * A;
+        }
+
+        return result;
+    }    
+}
---
+++ task29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход числа N, min, max. Затем генерирует массив и заполняет его случайными числами в диапозоне от min до max.
+
+Console.WriteLine("Введите размер массива");
+int N =int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите нижнюю границу диапазона заполнения");
+int min =int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите верхнюю границу диапазона заполнения");
+int max =int.Parse(Console.ReadLine()!);
+
+int[] arrayGener = GenerateArray(N, min, max);
+PrintArray(arrayGener);
+
+int[] GenerateArray(int size, int minBorder, int maxBorder)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minBorder, maxBorder);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
