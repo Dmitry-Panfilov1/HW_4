146a7a2ec6410c2f523e8d3c91944b040c75a8e9
Dmitry <panfilovdima1@yandex.ru>
2022-12-10 19:19:24 +0300

    task27 add

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+Console.WriteLine("Введите число");
+int number =int.Parse(Console.ReadLine()!);
+
+int sum = sumNumbers(number);
+Console.WriteLine(sum);
+
+int sumNumbers(int A)
+{
+    if (A < 0)
+    {
+        A = A * (-1);
+    }
+    
+    int summa = 0;
+
+    for (int i = A; i >= 1; i = i / 10)
+    {
+        int temp = i % 10;
+        summa = summa + temp;
+    }
+    return summa;
+}
